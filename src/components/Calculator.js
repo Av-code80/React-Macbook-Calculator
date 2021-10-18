12456f621d6e88a4eeb1bbacd61c175569291d5b
avehghasemian <av.ghasemian@gmail.com>
2021-10-18 23:29:11 +0200

    handlerClearData

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,95 +3,83 @@ import MainButton from "./MainButton";
 
 import styles from "./Calculator.module.css";
 
-
-
 function Calculator() {
   const [nextValue, setNextValue] = useState("0");
   const [prevValue, setPrevValue] = useState(null);
   const [operation, setOperation] = useState(null);
 
-useEffect(() => {}, [prevValue, nextValue, operation])
+  useEffect(() => {}, [prevValue, nextValue, operation]);
 
+  const handleNum = (number) => {
+    setNextValue(nextValue === "0" ? String(number) : nextValue + number);
+  };
 
-const handleNum = (number) => {
-  setNextValue(nextValue === "0" ? String(number) : nextValue + number)
-}
+  const handleDot = () => {
+    if (!/\./.test(nextValue)) {
+      setNextValue(nextValue + ".");
+    }
+  };
 
-
-const handleDot = () => {
-  if (!/\./.test(nextValue)) {
-    setNextValue(nextValue + ".")
-  }
-}
-
-const handlePercentage = () => {
-  setNextValue(parseFloat(nextValue) / 100);
+  const handlePercentage = () => {
+    setNextValue(parseFloat(nextValue) / 100);
     if (prevValue && nextValue === "") {
       setPrevValue(parseFloat(prevValue) / 100);
     }
-}
+  };
 
-const handleChangeSigne = () => {
-  setNextValue(parseFloat(nextValue) * -1)
-}
+  const handleChangeSigne = () => {
+    setNextValue(parseFloat(nextValue) * -1);
+  };
 
-const handleClearData = () => {
- setNextValue("0");
- setPrevValue("0");
+  const handleClearData = () => {
+    setNextValue("0");
+    setPrevValue("0");
+  };
 
- 
-}
+  const operationCalculator = {
+    "÷": (firstVal, secondVal) => firstVal / secondVal,
+    "×": (fisrtVal, secondVal) => fisrtVal * secondVal,
+    "+": (fisrtVal, secondVal) => fisrtVal + secondVal,
+    "-": (firstVal, secondVal) => firstVal - secondVal,
+    "=": (firstVal, secondVal) => secondVal,
+  };
 
-const operationCalculator = {
-  "÷": (firstVal, secondVal) => firstVal / secondVal,
-  "×": (fisrtVal, secondVal) => fisrtVal * secondVal,
-  "+": (fisrtVal, secondVal) => fisrtVal + secondVal,
-  "-": (firstVal, secondVal) => firstVal - secondVal,
-  "=": (firstVal, secondVal) => secondVal,
-};
+  const handleOperation = () => {
+    const res = operationCalculator[operation](
+      parseFloat(prevValue),
+      parseFloat(nextValue)
+    );
 
-const handleOperation = () => {
-   const res = operationCalculator[operation] (
-     parseFloat(prevValue),
-     parseFloat(nextValue))
+    setOperation(true);
+    setNextValue(String(res));
+    setPrevValue(null);
+  };
 
-     setOperation(true)
-     setNextValue(String(res))
-     setPrevValue(null)
-}
-
-
-const handlerFunction = (value) => {
-  if (Number.isInteger(value)) {
-    handleNum(parseInt(value, 10))
-  }
-  else if ( value in operationCalculator) {
-    if (operation === null) {
-      setOperation(value)
-      setPrevValue(nextValue)
-      setNextValue("")
+  const handlerFunction = (value) => {
+    if (Number.isInteger(value)) {
+      handleNum(parseInt(value, 10));
+    } else if (value in operationCalculator) {
+      if (operation === null) {
+        setOperation(value);
+        setPrevValue(nextValue);
+        setNextValue("");
+      }
+      if (operation) {
+        setOperation(value);
+      }
+      if (prevValue && nextValue && operation) {
+        handleOperation();
+      } else if (value === "AC") {
+        handleClearData();
+      } else if (value === "+/-") {
+        handleChangeSigne();
+      } else if (value === ".") {
+        handleDot();
+      } else if (value === "%") {
+        handlePercentage();
+      }
     }
-    if (operation) {
-      setOperation(value)
-    }
-    if (prevValue && nextValue && operation) {
-        handleOperation()
-    }
-    else if (value === "AC") {
-      handleClearData()
-    }
-    else if (value === "+/-") {
-      handleChangeSigne()
-    }
-    else if (value === ".") {
-      handleDot()
-    }
-    else if (value === "%") {
-      handlePercentage()
-    }
-  }
-}
-
+  };
 
   return (
     <>
@@ -101,16 +89,16 @@ const handlerFunction = (value) => {
         </div>
         <div className={styles["calculator-Keyboard"]}>
           <div className={styles.keysFunction}>
-            <MainButton keyValue={"AC"} onClick={handlerFunction} />
-            <MainButton keyValue={"+/-"} onClick={handlerFunction} />
-            <MainButton keyValue={"%"} onClick={handlerFunction} />
+            <MainButton nameOfClass={styles.functionColor} keyValue={"AC"} onClick={handlerFunction} />
+            <MainButton nameOfClass={styles.functionColor} keyValue={"+/-"} onClick={handlerFunction} />
+            <MainButton nameOfClass={styles.functionColor} keyValue={"%"} onClick={handlerFunction} />
           </div>
           <div className={styles.keysOperation}>
-            <MainButton keyValue={"÷"} onClick={handlerFunction} />
-            <MainButton keyValue={"×"} onClick={handlerFunction} />
-            <MainButton keyValue={"-"} onClick={handlerFunction} />
-            <MainButton keyValue={"+"} onClick={handlerFunction} />
-            <MainButton keyValue={"="} onClick={handlerFunction} />
+            <MainButton nameOfClass={styles.orangeOperation} keyValue={"÷"} onClick={handlerFunction} />
+            <MainButton nameOfClass={styles.orangeOperation} keyValue={"×"} onClick={handlerFunction} />
+            <MainButton nameOfClass={styles.orangeOperation} keyValue={"-"} onClick={handlerFunction} />
+            <MainButton nameOfClass={styles.orangeOperation} keyValue={"+"} onClick={handlerFunction} />
+            <MainButton nameOfClass={styles.orangeOperation} keyValue={"="} onClick={handlerFunction} />
           </div>
           <div className={styles.keysDigits}>
             <MainButton keyValue={7} onClick={handlerFunction} />
@@ -123,12 +111,13 @@ const handlerFunction = (value) => {
             <MainButton keyValue={2} onClick={handlerFunction} />
             <MainButton keyValue={3} onClick={handlerFunction} />
             <MainButton
+              nameOfClass={styles.keyZero}
               keyValue={0}
               onClick={handlerFunction}
             />
             <MainButton
               keyValue={"."}
-              className={styles.keyDat}
+              nameOfClass={styles.keyDat}
               onClick={handlerFunction}
             />
           </div>
@@ -138,4 +127,4 @@ const handlerFunction = (value) => {
   );
 }
 
-export default Calculator
+export default Calculator;
--- src/components/MainButton.js
+++ src/components/MainButton.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
-import styles from './MainButton.module.css'
+import './MainButton.module.css';
 
 
 const MainButton = (props) => {
 // e.target.value ??
 
   return (
-   
-      <button
-        className={`${styles["props.className"]}`}
-        type="button"
-        onClick={() => props.onClick(props.keyValue)}
-      >
-        {props.keyValue}
-        {""}
-      </button>
- 
+    <button
+      className={`${props.nameOfClass}`}
+      type="button"
+      onClick={() => props.onClick(props.keyValue)}
+    >
+      {props.keyValue}
+      {""}
+    </button>
   );
 }
 
 export default MainButton
+
+//  className={`${styles["props.className"]}`}
